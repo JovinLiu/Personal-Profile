fe07a518c978429a39f4cecda22ff22c416574f5
Jovin Liu <liujovin@gmail.com>
2024-10-20 13:10:37 +1100

    20241020

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -179,7 +179,7 @@ img {
 @keyframes moveUpFadeIn {
     0% {
         opacity: 0; 
-        transform: translateY(75%);
+        transform: translateY(25%);
     }
     100% {
         opacity: 1; 
